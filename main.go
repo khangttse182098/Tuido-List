cfc558691ec79980fde2794b3260726c15247119
khangttse182098 <khangttse182098@fpt.edu.vn>
2025-04-15 18:44:25 +0700

    implemented update & delete task

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -30,19 +31,48 @@ func checkErr(e error) {
 	}
 }
 
+func clearTerminal() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func saveTasksToFile(taskList []Task) {
+	tasksJson, err := json.MarshalIndent(taskList, "", "  ")
+	checkErr(err)
+	err = os.WriteFile("tasks.json", tasksJson, 0644)
+	checkErr(err)
+}
+
 func handleChooseTasks(option *int) {
 	for 1 == 1 {
-		fmt.Print("(1) list out all tasks\n(2) add new task\n(3) Exit\n")
+		fmt.Print(
+			"(1) list out all tasks\n(2) add new task\n(3) Edit a task\n(4) Delete a task\n(5) Exit\n",
+		)
 		fmt.Print("What do you want to do: ")
 		fmt.Scan(option)
 
 		// if they are then break
-		if *option >= 1 && *option <= 3 {
-			fmt.Print("\033[H\033[2J")
+		if *option >= 1 && *option <= 5 {
+			clearTerminal()
 			break
 		} else {
-			fmt.Print("\033[H\033[2J")
-			fmt.Println("You must enter option from 1-3!")
+			clearTerminal()
+			fmt.Println("You must enter option from 1-5!")
+		}
+	}
+}
+
+func printTaskList(taskList []Task) {
+	if len(taskList) == 0 {
+		fmt.Println("Todo list is empty!")
+	} else {
+		// count done tasks
+		fmt.Println("Todo List")
+		for i := range taskList {
+			if taskList[i].IsDone {
+				fmt.Printf("%d. [x] %s\n", i+1, taskList[i].Name)
+			} else {
+				fmt.Printf("%d. [ ] %s\n", i+1, taskList[i].Name)
+			}
 		}
 	}
 }
@@ -71,14 +101,14 @@ func listAllTasks(taskList []Task) bool {
 
 	//quit to menu
 	if chooseTask == 0 {
-		fmt.Print("\033[H\033[2J")
+		clearTerminal()
 		return false
 	}
 
 	// check if task is already done or not
 	taskList[chooseTask-1].IsDone = !taskList[chooseTask-1].IsDone
 	if taskList[chooseTask-1].IsDone {
-		fmt.Print("\033[H\033[2J")
+		clearTerminal()
 		fmt.Println("Mark task as complete")
 		doneTaskCount += 1
 		// check if all tasks are finnished
@@ -86,16 +116,13 @@ func listAllTasks(taskList []Task) bool {
 			print(ascii)
 		}
 	} else {
-		fmt.Print("\033[H\033[2J")
+		clearTerminal()
 		fmt.Println("Mark task as incomplete")
 	}
 	fmt.Println("")
 
-	// store in file.json
-	tasksJson, err := json.Marshal(taskList)
-	checkErr(err)
-	err = os.WriteFile("tasks.json", tasksJson, 0644)
-	checkErr(err)
+	// save to json file
+	saveTasksToFile(taskList)
 
 	return true
 }
@@ -105,15 +132,64 @@ func addTask(taskList *[]Task, reader *bufio.Reader) {
 	newTask, _ := reader.ReadString('\n')
 	newTask = strings.TrimSpace(newTask)
 	*taskList = append(*taskList, Task{Name: newTask, IsDone: false})
-	fmt.Print("\033[H\033[2J")
+	clearTerminal()
 	fmt.Println("Added task successfully!")
 	fmt.Println("")
 
-	// write to json file
-	taskJson, err := json.Marshal(*taskList)
-	checkErr(err)
-	err = os.WriteFile("tasks.json", taskJson, 0644)
-	checkErr(err)
+	// save to json file
+	saveTasksToFile(*taskList)
+}
+
+func updateTask(taskList *[]Task, reader *bufio.Reader) bool {
+	var chooseTaskIndex int
+	printTaskList(*taskList)
+
+	fmt.Printf("Choose a task from (1-%d) to update or 0 to quit: ", len(*taskList))
+	fmt.Scan(&chooseTaskIndex)
+	if chooseTaskIndex == 0 {
+		return false
+	}
+
+	fmt.Printf("Enter new name: ")
+	newTaskName, _ := reader.ReadString('\n')
+	newTaskName = strings.TrimSpace(newTaskName)
+	(*taskList)[chooseTaskIndex-1].Name = newTaskName
+
+	clearTerminal()
+	fmt.Println("Change the name successfully!")
+
+	//save to json file
+	saveTasksToFile(*taskList)
+
+	return true
+}
+
+func deleteTask(taskList *[]Task) bool {
+	var chooseTaskIndex int
+	printTaskList(*taskList)
+
+	fmt.Printf("Choose a task from (1-%d) to delete or 0 to quit: ", len(*taskList))
+	fmt.Scan(&chooseTaskIndex)
+	if chooseTaskIndex == 0 {
+		return false
+	}
+
+	var confirmChoice byte
+	fmt.Printf("Are you sure? (y/n): ")
+	fmt.Scan(&confirmChoice)
+	if confirmChoice == 'n' {
+		return false
+	}
+	*taskList = slices.Delete(*taskList, chooseTaskIndex-1, chooseTaskIndex)
+
+	//print out success message
+	clearTerminal()
+	fmt.Println("Delete the task successfully!")
+
+	//save to json file
+	saveTasksToFile(*taskList)
+
+	return true
 }
 
 func main() {
@@ -123,11 +199,11 @@ func main() {
 
 	//read file
 	fileContent, err := os.ReadFile("tasks.json")
-	checkErr(err)
-
-	//unmarshal json to taskList
-	err = json.Unmarshal(fileContent, &taskList)
-	checkErr(err)
+	if err == nil {
+		//unmarshal json to taskList
+		err = json.Unmarshal(fileContent, &taskList)
+		checkErr(err)
+	}
 
 	fmt.Println("Welcome to Terminal Todo List!")
 	for 1 == 1 {
@@ -141,7 +217,16 @@ func main() {
 			}
 		} else if option == 2 {
 			addTask(&taskList, reader)
+
 		} else if option == 3 {
+			if !updateTask(&taskList, reader) {
+				continue
+			}
+		} else if option == 4 {
+			if !deleteTask(&taskList) {
+				continue
+			}
+		} else if option == 5 {
 			fmt.Println("Bye byeee!")
 			break
 		}
