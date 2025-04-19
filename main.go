aca02f574078e541c6dda2160bc2e5675ceb46fa
khangttse182098 <khangttse182098@fpt.edu.vn>
2025-04-20 00:33:31 +0700

    feat: add new task & save to json file

--- main.go
+++ main.go
@@ -22,169 +22,11 @@ ______________________
     > ^ <
 `
 
-//	type Task struct {
-//		Name   string `json:"name"`
-//		IsDone bool   `json:"isDone"`
-//	}
 func checkErr(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
-
-//	func clearTerminal() {
-//		fmt.Print("\033[H\033[2J")
-//	}
-//
-// func handleChooseTasks(option *int) {
-// 	for 1 == 1 {
-// 		fmt.Print(
-// 			"(1) list out all tasks\n(2) add new task\n(3) Edit a task\n(4) Delete a task\n(5) Exit\n",
-// 		)
-// 		fmt.Print("What do you want to do: ")
-// 		fmt.Scan(option)
-//
-// 		// if they are then break
-// 		if *option >= 1 && *option <= 5 {
-// 			clearTerminal()
-// 			break
-// 		} else {
-// 			clearTerminal()
-// 			fmt.Println("You must enter option from 1-5!")
-// 		}
-// 	}
-// }
-//
-// func printTaskList(taskList []Task) {
-// 	if len(taskList) == 0 {
-// 		fmt.Println("Todo list is empty!")
-// 	} else {
-// 		// count done tasks
-// 		fmt.Println("Todo List")
-// 		for i := range taskList {
-// 			if taskList[i].IsDone {
-// 				fmt.Printf("%d. [x] %s\n", i+1, taskList[i].Name)
-// 			} else {
-// 				fmt.Printf("%d. [ ] %s\n", i+1, taskList[i].Name)
-// 			}
-// 		}
-// 	}
-// }
-//
-// func listAllTasks(taskList []Task) bool {
-// 	var doneTaskCount int
-// 	if len(taskList) == 0 {
-// 		fmt.Println("Todo list is empty!")
-// 	} else {
-// 		// count done tasks
-// 		fmt.Println("Todo List")
-// 		for i := range taskList {
-// 			if taskList[i].IsDone {
-// 				fmt.Printf("%d. [x] %s\n", i+1, taskList[i].Name)
-// 				doneTaskCount += 1
-// 			} else {
-// 				fmt.Printf("%d. [ ] %s\n", i+1, taskList[i].Name)
-// 			}
-// 		}
-// 	}
-//
-// 	// choose task to complete or incomplete
-// 	var chooseTask int
-// 	fmt.Printf("Choose a task to perform action (1-%d) or 0 to quit: ", len(taskList))
-// 	fmt.Scan(&chooseTask)
-//
-// 	//quit to menu
-// 	if chooseTask == 0 {
-// 		clearTerminal()
-// 		return false
-// 	}
-//
-// 	// check if task is already done or not
-// 	taskList[chooseTask-1].IsDone = !taskList[chooseTask-1].IsDone
-// 	if taskList[chooseTask-1].IsDone {
-// 		clearTerminal()
-// 		fmt.Println("Mark task as complete")
-// 		doneTaskCount += 1
-// 		// check if all tasks are finnished
-// 		if doneTaskCount == len(taskList) {
-// 			print(ascii)
-// 		}
-// 	} else {
-// 		clearTerminal()
-// 		fmt.Println("Mark task as incomplete")
-// 	}
-// 	fmt.Println("")
-//
-// 	// save to json file
-// 	saveTasksToFile(taskList)
-//
-// 	return true
-// }
-//
-// func addTask(taskList *[]Task, reader *bufio.Reader) {
-// 	fmt.Print("Enter task name: ")
-// 	newTask, _ := reader.ReadString('\n')
-// 	newTask = strings.TrimSpace(newTask)
-// 	*taskList = append(*taskList, Task{Name: newTask, IsDone: false})
-// 	clearTerminal()
-// 	fmt.Println("Added task successfully!")
-// 	fmt.Println("")
-//
-// 	// save to json file
-// 	saveTasksToFile(*taskList)
-// }
-//
-// func updateTask(taskList *[]Task, reader *bufio.Reader) bool {
-// 	var chooseTaskIndex int
-// 	printTaskList(*taskList)
-//
-// 	fmt.Printf("Choose a task from (1-%d) to update or 0 to quit: ", len(*taskList))
-// 	fmt.Scan(&chooseTaskIndex)
-// 	if chooseTaskIndex == 0 {
-// 		return false
-// 	}
-//
-// 	fmt.Printf("Enter new name: ")
-// 	newTaskName, _ := reader.ReadString('\n')
-// 	newTaskName = strings.TrimSpace(newTaskName)
-// 	(*taskList)[chooseTaskIndex-1].Name = newTaskName
-//
-// 	clearTerminal()
-// 	fmt.Println("Change the name successfully!")
-//
-// 	//save to json file
-// 	saveTasksToFile(*taskList)
-//
-// 	return true
-// }
-//
-// func deleteTask(taskList *[]Task) bool {
-// 	var chooseTaskIndex int
-// 	printTaskList(*taskList)
-//
-// 	fmt.Printf("Choose a task from (1-%d) to delete or 0 to quit: ", len(*taskList))
-// 	fmt.Scan(&chooseTaskIndex)
-// 	if chooseTaskIndex == 0 {
-// 		return false
-// 	}
-//
-// 	var confirmChoice byte
-// 	fmt.Printf("Are you sure? (y/n): ")
-// 	fmt.Scan(&confirmChoice)
-// 	if confirmChoice == 'n' {
-// 		return false
-// 	}
-// 	*taskList = slices.Delete(*taskList, chooseTaskIndex-1, chooseTaskIndex)
-//
-// 	//print out success message
-// 	clearTerminal()
-// 	fmt.Println("Delete the task successfully!")
-//
-// 	//save to json file
-// 	saveTasksToFile(*taskList)
-//
-// 	return true
-// }
 
 // ----------------------------------------------------------
 
@@ -196,9 +38,11 @@ const (
 )
 
 type model struct {
-	TaskList []string         `json:"taskList"`
-	Selected map[int]struct{} `json:"selected"`
-	cursor   int
+	TaskList     []string         `json:"taskList"`
+	Selected     map[int]struct{} `json:"selected"`
+	cursor       int
+	isAdd        bool
+	newTaskInput string
 }
 
 func initialModel() model {
@@ -225,26 +69,56 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl + c", "q":
-			saveTasksToFile(m)
-			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+	switch m.isAdd {
+	case true:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.Type {
+			case tea.KeyRunes:
+				m.newTaskInput += string(msg.Runes)
+			case tea.KeySpace:
+				m.newTaskInput += " "
+			case tea.KeyBackspace:
+				if len(m.newTaskInput) > 0 {
+					m.newTaskInput = m.newTaskInput[:len(m.newTaskInput)-1]
+				}
+			case tea.KeyEnter:
+				if len(m.newTaskInput) > 0 {
+					m.TaskList = append(m.TaskList, m.newTaskInput)
+					m.isAdd = false
+					m.newTaskInput = ""
+				}
+			case tea.KeyEsc:
+				saveTasksToFile(m)
+				return m, tea.Quit
 			}
-		case "down", "j":
-			if m.cursor < len(m.TaskList)-1 {
-				m.cursor++
-			}
-		case "enter", " ":
-			_, ok := m.Selected[m.cursor]
-			if ok {
-				delete(m.Selected, m.cursor)
-			} else {
-				m.Selected[m.cursor] = struct{}{}
+		}
+	case false:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "ctrl + c", "q":
+				saveTasksToFile(m)
+				return m, tea.Quit
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+			case "down", "j":
+				if m.cursor < len(m.TaskList)-1 {
+					m.cursor++
+				}
+			case "enter", " ":
+				_, ok := m.Selected[m.cursor]
+				if ok {
+					delete(m.Selected, m.cursor)
+				} else {
+					m.Selected[m.cursor] = struct{}{}
+				}
+			case "o":
+				if !m.isAdd {
+					m.isAdd = true
+				}
 			}
 		}
 	}
@@ -274,6 +148,11 @@ func (m model) View() string {
 		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, task)
 	}
 
+	// Is adding new task function on?
+	if m.isAdd {
+		s += fmt.Sprintf("  [ ] %s\n", m.newTaskInput)
+	}
+
 	// The footer
 	s += "\nPress q to quit.\n"
 
@@ -294,43 +173,4 @@ func main() {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
-
-	// reader := bufio.NewReader(os.Stdin)
-	// var option int
-	// var taskList []Task
-	//
-	// //read file
-	// fileContent, err := os.ReadFile("tasks.json")
-	// if err == nil {
-	// 	//unmarshal json to taskList
-	// 	err = json.Unmarshal(fileContent, &taskList)
-	// 	checkErr(err)
-	// }
-	//
-	// fmt.Println("Welcome to Terminal Todo List!")
-	// for 1 == 1 {
-	// 	// prompt the user to choose task
-	// 	handleChooseTasks(&option)
-	//
-	// 	// list out all tasks
-	// 	if option == 1 {
-	// 		if !listAllTasks(taskList) {
-	// 			continue
-	// 		}
-	// 	} else if option == 2 {
-	// 		addTask(&taskList, reader)
-	//
-	// 	} else if option == 3 {
-	// 		if !updateTask(&taskList, reader) {
-	// 			continue
-	// 		}
-	// 	} else if option == 4 {
-	// 		if !deleteTask(&taskList) {
-	// 			continue
-	// 		}
-	// 	} else if option == 5 {
-	// 		fmt.Println("Bye byeee!")
-	// 		break
-	// 	}
-	// }
 }
